47db319779fbd8398e13953cb88c560afb06e815
murilooo2009-eng <murilooo2009@gmail.com>
2026-02-08 14:54:07 -0300

    modulos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { EmpresaModule } from './empresa/empresa.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ChecklistModule } from './checklist/checklist.module';
+import { ExecucaoModule } from './execucao/execucao.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, EmpresaModule, UsuarioModule, ChecklistModule, ExecucaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
